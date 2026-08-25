9b73e147296d349fef302c1cb0bec58a0473dc56
Nimit Sharma <nimitsharma111@gmail.com>
2026-08-25 14:24:46 +0530

    Practice Set 1 - Q3

---
+++ Practice Set 1/S1Q3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,fact=1;
+    cout<<"Enter a Number: ";
+    cin>>n;
+
+    for(int i=1;i<=n;i++){
+        fact=fact*i;
+    }
+    cout<<"The factorial of "<<n<<" is: "<<fact<<endl;
+    return 0;
+
+
+}
