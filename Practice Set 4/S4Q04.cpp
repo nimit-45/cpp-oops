2fda3d00f947b43d8e28328d0d8042e1a8a297e5
Nimit Sharma <nimitsharma111@gmail.com>
2026-09-16 01:46:39 +0530

    Practice Set 4 - Q4

---
+++ Practice Set 4/S4Q04.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Distance{
+    public:
+    int Feet;
+    int Inches;
+    void setDetails(){
+        cout<<"Enter Feet : ";
+        cin>>Feet;
+        cout<<"Enter Inches : ";
+        cin>>Inches;
+    }
+    Distance operator+(const Distance &d2)const{
+        Distance d3;
+        d3.Feet=Feet+d2.Feet;
+        d3.Inches=Inches+d2.Inches;
+        if(d3.Inches>=12){
+            d3.Feet+=d3.Inches/12;
+            d3.Inches=d3.Inches%12;
+        }
+        return d3;
+    }
+};
+int main(){
+    Distance d1,d2,d3;
+    d1.setDetails();
+    d2.setDetails();
+    d3=d1+d2;
+    cout<<"Sum is = "<<d3.Feet<<" ft "<<d3.Inches<<" in";
+    return 0;
+}
